a060d90f594c38e25d11348a4736eab7c0e44f59
Oleksii <o.pruzhanskiy@gmail.com>
2025-06-03 14:06:07 +0300

    first layout

---
+++ src/components/NoteList/NoteList.tsx
@@ -0,0 +1,19 @@
+const NoteList = () => {
+  return (
+    <div>
+      <ul>
+        <li>note 1</li>
+        <li>note 2</li>
+        <li>note 3</li>
+        <li>note 4</li>
+        <li>note 5</li>
+        <li>note 6</li>
+        <li>note 7</li>
+        <li>note 8</li>
+        <li>note 9</li>
+      </ul>
+    </div>
+  );
+};
+
+export default NoteList;
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,5 +1,13 @@
+import NoteList from "../NoteList/NoteList";
+
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <div>
+      <div className="w-64 border-r h-full p-4 overflow-y-auto">
+        <NoteList />
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
