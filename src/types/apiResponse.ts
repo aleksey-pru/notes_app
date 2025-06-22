ce9e035ccb53f4257c0a76856613b7a106a3f115
Oleksii <o.pruzhanskiy@gmail.com>
2025-06-22 15:54:52 +0300

    remove tiptap from package.json

--- src/types/apiResponse.ts
+++ src/types/apiResponse.ts
@@ -1,4 +1,4 @@
-import { JSONContent } from '@tiptap/react';
+import type { EditorState, RawDraftContentState } from 'draft-js';
 
 import type { RootState } from './store';
 
@@ -13,12 +13,12 @@ export type TNoteApiResponse = {
 export type TNote = {
   id: string;
   title: string;
-  content: string;
+  content: RawDraftContentState;
   createdAt: Date;
 };
 
 export type TNoteWithEditorState = Omit<TNote, 'content'> & {
-  content: JSONContent;
+  content: EditorState;
 };
 export type TNoteItem = {
   [id: string]: {
